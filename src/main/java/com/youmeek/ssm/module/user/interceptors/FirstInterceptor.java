dbaa0e80d7145fbab369c8c05f8295fb941d28f1
ryan <249900703@qq.com>
2016-05-26 09:05:02 +0800

    spring-mvc增加拦截器

---
+++ src/main/java/com/youmeek/ssm/module/user/interceptors/FirstInterceptor.java
@@ -0,0 +1,68 @@
+package com.youmeek.ssm.module.user.interceptors;
+
+
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("[FirstInterceptor] preHandle");
+        return true;
+//        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("[FirstInterceptor] postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("[FirstInterceptor] afterCompletion");
+    }
+//    /**
+//     * 该方法在目标方法之前被调用.
+//     * 若返回值为 true, 则继续调用后续的拦截器和目标方法.
+//     * 若返回值为 false, 则不会再调用后续的拦截器和目标方法.
+//     *
+//     * 可以考虑做权限. 日志, 事务等.
+//     */
+//    @Override
+//    public boolean preHandle(HttpServletRequest request,
+//                             HttpServletResponse response, Object handler) throws Exception {
+//        System.out.println("[FirstInterceptor] preHandle");
+//        return true;
+//    }
+//
+//    /**
+//     * 调用目标方法之后, 但渲染视图之前.
+//     * 可以对请求域中的属性或视图做出修改.
+//     */
+//    @Override
+//    public void postHandle(HttpServletRequest request,
+//                           HttpServletResponse response, Object handler,
+//                           ModelAndView modelAndView) throws Exception {
+//        System.out.println("[FirstInterceptor] postHandle");
+//    }
+//
+//    /**
+//     * 渲染视图之后被调用. 释放资源
+//     */
+//    @Override
+//    public void afterCompletion(HttpServletRequest request,
+//                                HttpServletResponse response, Object handler, Exception ex)
+//            throws Exception {
+//        System.out.println("[FirstInterceptor] afterCompletion");
+//    }
+
+}
